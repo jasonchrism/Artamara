ea98ceffc5ac52c93a064de7f39637d5d33e13a2
Jason Chrisbellno Mackenzie <jasoncm712@gmail.com>
2024-07-16 21:28:52 +0700

    feat: Order Detail - Admin

--- app/Http/Controllers/Admin/ReturnDetailController.php
+++ app/Http/Controllers/Admin/ReturnDetailController.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Order;
-use App\Models\OrderDetail;
 use App\Models\Refund;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Psy\CodeCleaner\ReturnTypePass;
 
 class ReturnDetailController extends Controller
 {
@@ -16,32 +15,17 @@ class ReturnDetailController extends Controller
         $order_id = '9c86777e-158d-4e78-8ca1-db7602ab5aa9';
         $orders = Order::with(['userAddress.user', 'userAddress.address', 'payment.paymentMethod', 'refund'])
             ->where('order_id', $order_id)
+            ->where('status', 'ADMIN REVIEW')
             ->get();
 
         $items = Order::with(['orderDetail.product.user'])
             ->where('order_id', $order_id)
+            ->where('status', 'ADMIN REVIEW')
             ->get();
 
-        return view('artist.return.returnDetail', [
+        return view('admin.return.returnDetail', [
             'orders' => $orders,
             'items' => $items
         ]);
-    }
-
-    public function appeal(Request $request) {
-        $order_id = $request->input('order_id');
-        
-        $refund = Refund::find($order_id);
-        
-        try {
-            DB::beginTransaction();
-            $refund->update([
-                'status' => 'ADMIN CONFIRMATION'
-            ]);
-            DB::commit();
-        } catch(Exception $e) {
-            DB::rollBack();
-        }
-        return redirect('/dashboard/artist/home');
     }
 }
