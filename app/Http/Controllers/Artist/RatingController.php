2f53ebcd7ad07925f99f8278debf1524be9eff35
Jason Chrisbellno Mackenzie <jasoncm712@gmail.com>
2024-07-17 10:25:42 +0700

    fix: conflict merge

--- app/Http/Controllers/Artist/RatingController.php
+++ app/Http/Controllers/Artist/RatingController.php
@@ -24,21 +24,16 @@ class RatingController extends Controller
                 ->get();
             return DataTables::of($data)
                 ->addIndexColumn()
-                ->addColumn('role', function($row){
-                    // Ngubah nilai role, misalkan dari "ADMIN" menjadi "Admin"
-                    return ucfirst(strtolower($row->role));
+                ->addColumn('order_id', function($row){
+                    return ucfirst(strtolower($row->order_id));
                 })
-                ->addColumn('status', function($row){
-                    // Ngubah nilai status, misalkan dari "ACTIVE" menjadi "Active"
-                    return ucfirst(strtolower($row->status));
+                ->addColumn('buyer', function($row){
+                    return ucfirst(strtolower($row->username));
                 })
                 // intinya ini untuk button per-row
                 ->addColumn('action', function($row){
-                    $detailsUrl = route('userDetails', $row->user_id);
-                    $deleteArtist = route('deleteArtist', $row->user_id);
-                    // intinya ini untuk balikin dropdown ke tables
+                    $detailsUrl = route('rating.show', $row->user_id);
                     $modalId = 'modal-' . $row->user_id;
-                    $csrfToken = csrf_token();
                     $actionBtn = '
                     <div class="dropdown">
                         <button class="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -57,32 +52,7 @@ class RatingController extends Controller
                         </button>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="' . $detailsUrl . '">Details</a></li>
-                            <li>
-                                <button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#' . $modalId . '">Delete</button>
-                            </li>
                         </ul>
-                    </div>
-
-                    <div class="modal fade" id="' . $modalId . '" tabindex="-1" aria-labelledby="' . $modalId . 'Label" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="header">
-                                    <h5 class="modal-title" id="' . $modalId . 'Label">Delete User</h5>
-                                </div>
-                                <div class="content-body-delete">
-                                    <p style="color: var(--bs-secondary-txt);">Are you sure to delete this user?</p>
-                                </div>
-                                <div class="footer-modal-delete">
-                                    <form action="' . $deleteArtist . '" method="post">
-                                    <input type="hidden" name="_token" value="' . $csrfToken . '">
-                                    <div class = "button-action-delete">
-                                        <button type="button" class="close" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary">Delete</button>
-                                    </div>
-                                </form>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                     ';
                     return $actionBtn;
