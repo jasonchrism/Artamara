d2cfd0424a50a6bb43d0cd2f2264ef1461df57bf
abdhy <abdhysamoedra030@gmail.com>
2024-07-24 20:47:00 +0700

    style : datatable style, several bug still exist xdd

--- app/Http/Controllers/Artist/TransactionController.php
+++ app/Http/Controllers/Artist/TransactionController.php
@@ -89,53 +89,100 @@ class TransactionController extends Controller
     }
     public function tabs(Request $request, $status)
     {
-        // Log::info("hello tabs");
         if ($request->ajax()) {
             $authUserId = Auth::user()->user_id;
             $data = Order::where('status', $status)
-                ->with(['orderDetail.product', 'payment.paymentMethod', 'user'])
+                ->with(['orderDetail.product', 'payment.paymentMethod', 'user' , 'refund'])
                 ->whereHas('orderDetail.product', function ($query) use ($authUserId) {
                     $query->where('user_id', $authUserId);
                 })
                 ->get();
-            Log::info($data);
-            return Datatables::of($data)
+
+            $datatable = Datatables::of($data)
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
                     $detailsUrl = route('artist-transactions.show', $row->order_id);
                     $modalId = 'modal-' . $row->order_id;
                     $csrfToken = csrf_token();
                     $actionBtn = '
-                <div class="dropdown">
-                    <button class="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0_219_3359)">
-                        <path
-                            d="M5 10C5.53043 10 6.03914 10.2107 6.41421 10.5858C6.78929 10.9609 7 11.4696 7 12C7 12.5304 6.78929 13.0391 6.41421 13.4142C6.03914 13.7893 5.53043 14 5 14C4.46957 14 3.96086 13.7893 3.58579 13.4142C3.21071 13.0391 3 12.5304 3 12C3 11.4696 3.21071 10.9609 3.58579 10.5858C3.96086 10.2107 4.46957 10 5 10ZM12 10C12.5304 10 13.0391 10.2107 13.4142 10.5858C13.7893 10.9609 14 11.4696 14 12C14 12.5304 13.7893 13.0391 13.4142 13.4142C13.0391 13.7893 12.5304 14 12 14C11.4696 14 10.9609 13.7893 10.5858 13.4142C10.2107 13.0391 10 12.5304 10 12C10 11.4696 10.2107 10.9609 10.5858 10.5858C10.9609 10.2107 11.4696 10 12 10ZM19 10C19.5304 10 20.0391 10.2107 20.4142 10.5858C20.7893 10.9609 21 11.4696 21 12C21 12.5304 20.7893 13.0391 20.4142 13.4142C20.0391 13.7893 19.5304 14 19 14C18.4696 14 17.9609 13.7893 17.5858 13.4142C17.2107 13.0391 17 12.5304 17 12C17 11.4696 17.2107 10.9609 17.5858 10.5858C17.9609 10.2107 18.4696 10 19 10Z"
-                            fill="#464646" />
-                        </g>
-                        <defs>
-                            <clipPath id="clip0_219_3359">
-                                <rect width="24" height="24" fill="white" />
-                            </clipPath>
-                        </defs>
-                    </svg>
-                    </button>
-                    <ul class="dropdown-menu custom-dropdown-menu">
-                        <li><a class="dropdown-item" href="' . $detailsUrl . '">Details</a></li>
-                    </ul>
-                </div>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_219_3359)">
+                            <path
+                                d="M5 10C5.53043 10 6.03914 10.2107 6.41421 10.5858C6.78929 10.9609 7 11.4696 7 12C7 12.5304 6.78929 13.0391 6.41421 13.4142C6.03914 13.7893 5.53043 14 5 14C4.46957 14 3.96086 13.7893 3.58579 13.4142C3.21071 13.0391 3 12.5304 3 12C3 11.4696 3.21071 10.9609 3.58579 10.5858C3.96086 10.2107 4.46957 10 5 10ZM12 10C12.5304 10 13.0391 10.2107 13.4142 10.5858C13.7893 10.9609 14 11.4696 14 12C14 12.5304 13.7893 13.0391 13.4142 13.4142C13.0391 13.7893 12.5304 14 12 14C11.4696 14 10.9609 13.7893 10.5858 13.4142C10.2107 13.0391 10 12.5304 10 12C10 11.4696 10.2107 10.9609 10.5858 10.5858C10.9609 10.2107 11.4696 10 12 10ZM19 10C19.5304 10 20.0391 10.2107 20.4142 10.5858C20.7893 10.9609 21 11.4696 21 12C21 12.5304 20.7893 13.0391 20.4142 13.4142C20.0391 13.7893 19.5304 14 19 14C18.4696 14 17.9609 13.7893 17.5858 13.4142C17.2107 13.0391 17 12.5304 17 12C17 11.4696 17.2107 10.9609 17.5858 10.5858C17.9609 10.2107 18.4696 10 19 10Z"
+                                fill="#464646" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_219_3359">
+                                    <rect width="24" height="24" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                        </button>
+                        <ul class="dropdown-menu custom-dropdown-menu">
+                            <li><a class="dropdown-item" href="' . $detailsUrl . '">Details</a></li>
+                        </ul>
+                    </div>
 
-                <div class="modal fade" id="' . $modalId . '" tabindex="-1" aria-labelledby="' . $modalId . 'Label" aria-hidden="true">
-                </div>
+                    <div class="modal fade" id="' . $modalId . '" tabindex="-1" aria-labelledby="' . $modalId . 'Label" aria-hidden="true">
+                    </div>
                 ';
                     return $actionBtn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
+                });
+
+            // Conditionally add the 'status' column if the status is 'returned'
+            if ($status == 'RETURNED') {
+                $datatable->addColumn('status', function ($row) {
+                    $status = ucfirst(strtolower($row->refund->status ?? 'Unknown'));
+
+                    // Define the background color and text color based on status
+                    switch ($status) {
+                        case 'Admin review':
+                            $backgroundColor = 'var(--bg-overlay-2)';
+                            $textColor = 'var(--text-secondary)';
+                            break;
+                        case 'Artist review':
+                            $backgroundColor = 'var(--bg-label-blue)';
+                            $textColor = '#95D3FF';
+                            break;
+                        case 'Admin confirmation':
+                            $backgroundColor = 'var(--bg-label-primary)';
+                            $textColor = 'green';
+                            break;
+                        case 'Rejected':
+                            $backgroundColor = 'var(--bg-label-primary)';
+                            $textColor = 'var(--primary)';
+                            break;
+                        case 'Accepted':
+                            $backgroundColor = 'var(--bg-label-primary)';
+                            $textColor = 'red';
+                            break;
+                        case 'Finished':
+                            $backgroundColor = 'var(--bg-label-primary)';
+                            $textColor = 'white';
+                            break;
+                        default:
+                            $backgroundColor = 'gray'; // Fallback color
+                            $textColor = 'white';
+                    }
+
+                    // Return the styled status
+                    return '
+                    <div class="status-label" style="background-color: ' . $backgroundColor . '; color: ' . $textColor . '; padding: 5px 10px; display: inline-block;">
+                        ' . htmlspecialchars($status, ENT_QUOTES, 'UTF-8') . '
+                    </div>
+                ';
+                });
+            }
+
+            // Render raw columns that contain raw HTML
+            return $datatable->rawColumns(['status', 'action'])->make(true);
         }
+
         return view('artist.transaction');
     }
+
 
     /**
      * Show the form for creating a new resou   rce.
@@ -271,7 +318,7 @@ class TransactionController extends Controller
         $order = Order::findOrFail($id);
 
         $order->receipt_number = $request->input('receipt_number');
-        
+
         if ($order->status === 'PACKING') {
             $order->status = 'SHIPPING';
         }
--- resources/views/admin/transaction.blade.php
+++ resources/views/admin/transaction.blade.php
@@ -11,7 +11,6 @@
     <div class="tab">
         <div style="margin-top: 62px;">
             <ul class="nav nav-pills" id="myTab">
-
                 <li class="nav-item tab-link tab-active">
                     <a class="tabs" href="{{ route('admintabs', 'CONFIRMED') }}"
                         data-ajax-url="{{ route('admintabs', 'CONFIRMED') }}">Confirmed</a>
@@ -20,15 +19,14 @@
                     <a class="tabs" href="{{ route('admintabs', 'RETURNED') }}"
                         data-ajax-url="{{ route('admintabs', 'RETURNED') }}">Returned</a>
                 </li>
-
             </ul>
         </div>
     </div>
     <div class="container packing-container">
-
         <table class="table table-borderless data-table packing-table">
             <thead class="packing-table-head">
-                <tr>
+                <tr id="table-headers">
+                    <!-- Default headers for CONFIRMED status -->
                     <th class="packing-table-title">No</th>
                     <th class="packing-table-title">Order ID</th>
                     <th class="packing-table-title">Buyer Name</th>
@@ -39,7 +37,6 @@
                 </tr>
             </thead>
         </table>
-
     </div>
     <script type="text/javascript">
         $(function() {
@@ -48,9 +45,9 @@
                 serverSide: true,
                 responsive: true,
                 ajax: {
-                    url: '{!! url()->current() !!}', // Default URL
+                    url: '{!! route('admintabs', 'CONFIRMED') !!}',
                     data: function(d) {
-                        d.status = 'FINISHED'; // Default status
+                        d.status = 'CONFIRMED';
                     }
                 },
                 columns: [{
@@ -85,17 +82,138 @@
                         name: 'action',
                         orderable: false,
                         searchable: false
-                    },
+                    }
                 ]
             });
-            // Handle tab click event
+
             $('a.tabs').on('click', function(e) {
                 e.preventDefault();
                 var ajaxUrl = $(this).data('ajax-url');
-                var status = $(this).text().toUpperCase(); // Assuming tab text is the status
+                var status = $(this).text().toUpperCase();
 
                 // Update DataTables AJAX with the new URL
                 table.ajax.url(ajaxUrl).load();
+
+                // Update the columns dynamically based on the status
+                if (status === 'RETURNED') {
+                    table.clear().destroy();
+                    $('#table-headers').html(`
+                        <th class="packing-table-title">No</th>
+                        <th class="packing-table-title">Order ID</th>
+                        <th class="packing-table-title">Buyer Name</th>
+                        <th class="packing-table-title">Request Date</th>
+                        <th class="packing-table-title">Status</th>
+                        <th class="packing-table-title" width="100px"></th>
+                    `);
+                    table = $('.data-table').DataTable({
+                        processing: true,
+                        serverSide: true,
+                        responsive: true,
+                        ajax: {
+                            url: ajaxUrl,
+                            data: function(d) {
+                                d.status = 'RETURNED';
+                            }
+                        },
+                        columns: [{
+                                data: null,
+                                name: 'no',
+                                render: function(data, type, row, meta) {
+                                    return meta.row + meta.settings._iDisplayStart + 1;
+                                }
+                            },
+                            {
+                                data: 'order_id',
+                                name: 'order_id'
+                            },
+                            {
+                                data: 'user.name',
+                                name: 'user.name'
+                            },
+                            {
+                                data: 'refund.created_at',
+                                name: 'refund.created_at',
+                                render: function(data, type, row) {
+                                    if (type === 'display') {
+                                        // Convert the ISO date string to a JavaScript Date object
+                                        let date = new Date(data);
+                                        // Format the date as needed, e.g., 'dd-mm-yyyy hh:mm:ss'
+                                        return date.toLocaleDateString('en-GB') + ' ' + date
+                                            .toLocaleTimeString('en-GB');
+                                    }
+                                    return data;
+                                }
+                            },
+                            {
+                                data: 'status',
+                                name: 'status'
+                            },
+                            {
+                                data: 'action',
+                                name: 'action',
+                                orderable: false,
+                                searchable: false
+                            }
+                        ]
+                    });
+                } else {
+                    table.clear().destroy();
+                    $('#table-headers').html(`
+                        <th class="packing-table-title">No</th>
+                        <th class="packing-table-title">Order ID</th>
+                        <th class="packing-table-title">Buyer Name</th>
+                        <th class="packing-table-title">End Date</th>
+                        <th class="packing-table-title">Payment Method</th>
+                        <th class="packing-table-title">Total Price</th>
+                        <th class="packing-table-title" width="100px"></th>
+                    `);
+                    table = $('.data-table').DataTable({
+                        processing: true,
+                        serverSide: true,
+                        responsive: true,
+                        ajax: {
+                            url: ajaxUrl,
+                            data: function(d) {
+                                d.status = 'CONFIRMED';
+                            }
+                        },
+                        columns: [{
+                                data: null,
+                                name: 'no',
+                                render: function(data, type, row, meta) {
+                                    return meta.row + meta.settings._iDisplayStart + 1;
+                                }
+                            },
+                            {
+                                data: 'order_id',
+                                name: 'order_id'
+                            },
+                            {
+                                data: 'user.name',
+                                name: 'user.name'
+                            },
+                            {
+                                data: 'end_date',
+                                name: 'end_date'
+                            },
+                            {
+                                data: 'payment.payment_method.name',
+                                name: 'payment.payment_method.name'
+                            },
+                            {
+                                data: 'total_price',
+                                name: 'total_price'
+                            },
+                            {
+                                data: 'action',
+                                name: 'action',
+                                orderable: false,
+                                searchable: false
+                            }
+                        ]
+                    });
+                }
+
                 // Remove active class from all tabs
                 $('li.nav-item').removeClass('tab-active');
 
