c6654b01435ddf2b9824bce398085832aa806729
satmika ozora <mikaozora08@gmail.com>
2024-07-30 13:36:23 +0700

    feat: integration product auction with landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -37,7 +37,7 @@
                             <img src="{{ $p->thumbnail }}" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $p->title }}</h5>
-                                <p class="card-desc">{{ $p->description }}</p>
+                                <p class="card-desc">{{ $p->user->name }}</p>
                                 <p class="card-desc">{{ $p->year }}</p>
                                 <h5 class="card-title" style="color: var(--primary); font-weight:400;">{{ $p->price }}
                                 </h5>
@@ -76,72 +76,30 @@
             <h3>View more</h3>
         </div>
         <div class="row align-items-end w-100" data-masonry='{"percentPosition": true }'>
-            <div class="col-md-2 mb-3">
-                <div class="card">
-                    <img src="assets/art1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sepi Penghujung</h5>
-                        <p class="card-desc">Joko Pinurbo</p>
-                        <p class="card-desc">2014</p>
-                        <h5 class="card-title" style="color: var(--primary); font-weight:400;">Rp 120.000.000</h5>
-                    </div>
+            @foreach ($auctions as $auction)
+                <div class="col-md-2 mb-3">
+                    <a href="/auctionDetails/{{ $auction->product_id }}">
+                        <div class="card">
+                            <img src="{{ $auction->thumbnail }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $auction->title }}</h5>
+                                <p class="card-desc">{{ $auction->user->name }}</p>
+                                <p class="card-desc">{{ $auction->year }}</p>
+                                <h5 class="card-title" style="color: var(--primary); font-weight:400;">
+                                    {{ 'Rp' . number_format(count($auction->productAuction->bid) == 0 ? $auction->start_price : $auction->productAuction->bid()->orderBy('bid_price', 'desc')->first()->bid_price, 0, ',', '.') }}
+                                </h5>
+                                <p class="card-desc"><span id="countdown-{{ $auction->product_id }}"></span></p>
+                                <span class="end-date"
+                                    style="display:none;">{{ $auction->productAuction->end_date }}</span>
+                                <span class="start-date"
+                                    style="display:none;">{{ $auction->productAuction->start_date }}</span>
+                                <span class="status" style="display:none;">{{ $auction->productAuction->status }}</span>
+                                <span class="product_id" style="display:none;">{{ $auction->product_id }}</span>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-            </div>
-            <div class="col-md-2 mb-3">
-                <div class="card">
-                    <img src="assets/art2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sepi Penghujung</h5>
-                        <p class="card-desc">Joko Pinurbo</p>
-                        <p class="card-desc">2014</p>
-                        <h5 class="card-title" style="color: var(--primary); font-weight:400;">Rp 120.000.000</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-2 mb-3">
-                <div class="card">
-                    <img src="assets/art3.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sepi Penghujung</h5>
-                        <p class="card-desc">Joko Pinurbo</p>
-                        <p class="card-desc">2014</p>
-                        <h5 class="card-title" style="color: var(--primary); font-weight:400;">Rp 120.000.000</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-2 mb-3">
-                <div class="card">
-                    <img src="assets/art4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sepi Penghujung</h5>
-                        <p class="card-desc">Joko Pinurbo</p>
-                        <p class="card-desc">2014</p>
-                        <h5 class="card-title" style="color: var(--primary); font-weight:400;">Rp 120.000.000</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-2 mb-3">
-                <div class="card">
-                    <img src="assets/art3.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sepi Penghujung</h5>
-                        <p class="card-desc">Joko Pinurbo</p>
-                        <p class="card-desc">2014</p>
-                        <h5 class="card-title" style="color: var(--primary); font-weight:400;">Rp 120.000.000</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-2 mb-3">
-                <div class="card">
-                    <img src="assets/art4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Sepi Penghujung</h5>
-                        <p class="card-desc">Joko Pinurbo</p>
-                        <p class="card-desc">2014</p>
-                        <h5 class="card-title" style="color: var(--primary); font-weight:400;">Rp 120.000.000</h5>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
 
     </section>
@@ -153,5 +111,55 @@
 @push('after-scripts')
     <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
         integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async>
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            // Function to initialize countdown
+            function initializeCountdown(productId, date, status) {
+                var countdownElement = document.getElementById('countdown-' + productId);
+                var dateTime = new Date(date).getTime();
+                let preText = "";
+                var countdownInterval = setInterval(function() {
+                    var now = new Date().getTime();
+                    var distance = dateTime - now;
+                    if (status === "STARTING SOON") {
+                        preText = "Starts in ";
+                    } else {
+                        preText = "Ends in ";
+                    }
+
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                    countdownElement.innerHTML = preText + days + "d : " + hours + "h : " + minutes +
+                        "m : " + seconds +
+                        "s ";
+
+                    if (distance < 0) {
+                        clearInterval(countdownInterval);
+                        countdownElement.innerHTML = "Ended";
+                    }
+                }, 1000);
+            }
+
+            // Initialize countdown for each product
+            var products = document.querySelectorAll('.col-md-2');
+            products.forEach(function(product) {
+                var endDate = product.querySelector('.end-date').textContent;
+                var startDate = product.querySelector('.start-date').textContent;
+                var status = product.querySelector('.status').textContent;
+                var productId = product.querySelector('.product_id').textContent;
+
+                let date = null
+                if (status === "STARTING SOON") {
+                    date = startDate;
+                } else {
+                    date = endDate;
+                }
+                initializeCountdown(productId, date, status);
+            });
+        });
     </script>
 @endpush
